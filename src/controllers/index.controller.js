a612d1e0c4742d19872a11ec5700d47ff3e27bc8
Gabriel-Baquerizo <gabriel.baquerizo@kfc.com.ec>
2025-04-13 19:27:27 -0500

    Query test 2

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../database/connection.js";
 
 export const indexQuery = async (req, res) => {
     const [RESULT] = await pool.query(
-        'SELECT "Gabriel" AS USUARIO; RENAME TABLE restaurantes TO cambioaplicado;'
+        'RENAME TABLE restaurantes TO cambioaplicado;'
     )
     res.json(RESULT[0]);
 }
